230512d388d1c5d960652437f7c3379814ab210c
oleksii.kashtanov <oleksii.kashtanov@efumo.lv>
2026-08-28 01:23:06 +0300

    feat(soap): implement WSDL autodiscovery and SoapServer handling with classmap

--- src/Controller/SoapController.php
+++ src/Controller/SoapController.php
@@ -2,69 +2,70 @@
 
 namespace App\Controller;
 
-use App\Dto\SoapOrderRequestInterface;
+use App\Dto\CreateEmptyOrderRequestDto;
+use App\Soap\OrderSoapFacade;
+use Laminas\Soap\AutoDiscover;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use App\Soap\OrderSoapFacade;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Laminas\Soap\AutoDiscover;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Controller to handle SOAP requests for order creation and management.
  */
 final class SoapController extends AbstractController
 {
-    /**
-     * SoapController constructor.
-     */
     public function __construct(
-        private OrderSoapFacade $orderFacade
-    ) {
-    }
+        private readonly OrderSoapFacade $orderFacade
+    ) {}
 
     /**
      * Handle incoming SOAP XML requests, process order payload, and return SOAP XML response.
      *
-     * @param SoapOrderRequestInterface $query The deserialized request payload DTO.
-     * @return Response A SOAP response containing the created/updated order ID or a SOAP Fault.
+     * @param Request $request The incoming HTTP request containing SOAP XML.
+     * @param UrlGeneratorInterface $urlGenerator The URL generator for creating WSDL endpoint URL.
+     *
+     * @return Response The HTTP response containing SOAP XML or WSDL.
      */
     public function run(
-        string $service,
-        Request $request
+        Request $request,
+        string $service = 'orders'
     ): Response {
         $endpointUrl = $this->generateUrl(
-            'soap_endpoint',
+            'api_soap_orders',
             [],
             UrlGeneratorInterface::ABSOLUTE_URL
         );
 
-        // If the client requested a WSDL (GET /soap/orders?wsdl)
-        if ($request->query->has('wsdl')) {
-            $autodiscover = new AutoDiscover();
-            // Pass the name of the facade class.
-            $autodiscover->setClass(get_class($this->orderFacade));
-            // We specify the URL where the client should send POST requests.
-            $autodiscover->setUri($endpointUrl);
-            $autodiscover->setServiceName(ucfirst($service) . 'Service');
+        $autodiscover = new AutoDiscover();
+        $autodiscover->setClass(get_class($this->orderFacade));
+        $autodiscover->setUri($endpointUrl);
+        $autodiscover->setServiceName(ucfirst($service) . 'Service');
+        $wsdlXml = $autodiscover->toXml();
 
+        if ($request->query->has('wsdl')) {
             return new Response(
-                $autodiscover->toXml(),
+                $wsdlXml,
                 200,
                 ['Content-Type' => 'text/xml; charset=UTF-8']
             );
         }
 
-        // Processing a live SOAP request (POST /soap/orders)
-        // For simplicity, we'll use non-WSDL mode, since we just generated it ourselves.
-        $soapServer = new \SoapServer(null, ['uri' => $endpointUrl]);
+        $wsdlPath = sys_get_temp_dir() . '/tile_market_orders.wsdl';
+        file_put_contents($wsdlPath, $wsdlXml);
+
+        $soapServer = new \SoapServer($wsdlPath, [
+            'classmap' => [
+                'CreateEmptyOrderRequestDto' => CreateEmptyOrderRequestDto::class,
+            ],
+        ]);
         $soapServer->setObject($this->orderFacade);
 
         $response = new Response();
         $response->headers->set('Content-Type', 'text/xml; charset=UTF-8');
 
         ob_start();
-        $soapServer->handle();
+        @$soapServer->handle($request->getContent());
         $response->setContent(ob_get_clean());
 
         return $response;
